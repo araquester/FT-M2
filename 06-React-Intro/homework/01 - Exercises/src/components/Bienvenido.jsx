abac700568a5e9ae5a1335a32831a5e4e2b6a379
Mauricio Araque <111613206+araquester@users.noreply.github.com>
2022-11-28 19:20:53 -0500

    Modificado el 28_11_2022

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Mauricio Araque";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Tarea Unidad 6 - Ejercicio 01</h1>
+      <h3>{studentName}</h3>
+      <ul>
+      {techSkills.map(Skill=>
+      (<li>{Skill}</li>)
+    )}
+      </ul>
+      <Botones alerts={alerts}/>
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
